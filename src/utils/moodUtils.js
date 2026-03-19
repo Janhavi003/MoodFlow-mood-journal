ade19d79c9303a2369ded37cf3a327d8fab9c3b3
Janhavi Hivarekar <144115237+Janhavi003@users.noreply.github.com>
2026-03-20 00:06:40 +0530

    Update emoji for mood

--- src/utils/moodUtils.js
+++ src/utils/moodUtils.js
@@ -1,6 +1,6 @@
 export const moods = [
   { label: "Happy", emoji: "😄", color: "bg-moodHappy" },
-  { label: "Good", emoji: "🙂", color: "bg-moodGood" },
+  { label: "Good", emoji: "😊", color: "bg-moodGood" },
   { label: "Neutral", emoji: "😐", color: "bg-moodNeutral" },
   { label: "Sad", emoji: "😔", color: "bg-moodSad" },
   { label: "Angry", emoji: "😡", color: "bg-moodAngry" },
